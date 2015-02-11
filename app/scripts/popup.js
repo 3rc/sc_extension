dc07c5b01421e13953da7d9b054e02cd52012eda
3rc <thedj3rc@gmail.com>
2015-02-11 16:56:22 -0700

    Hide Reposts functionality completed

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,3 +1,41 @@
-'use strict';
+if(window.location.toString() == "https://soundcloud.com/stream") {
 
-console.log('\'Allo \'Allo! Popup');
+  var repostTitles = [];
+  var repostsRemoved = false;
+  setTimeout(function() {
+  console.log("★★★★★★SoundCloud Enhancer loaded!★★★★★★");
+    $("header").append("<div class='moreoptions'>★★&nbsp;<button id='remove-reposts'>Remove Reposts</button></div>");
+    $(".l-content").append("<div class='stream-reposts'></div>");
+    $("#remove-reposts").click(function() {
+      removeReposts($(".stream"))
+    })
+  }, 3000)
+  function removeReposts() {
+    var repostsRemoved = true;
+    $(".stream-reposts").append($(".sound__header:contains('Reposted')").parent().parent().parent());
+
+    repostTitles = [];
+    var reposts = $(".stream-reposts").find(".soundTitle__title");
+
+    for( i = 0; i < reposts.length; i++) {
+      repostTitles.push(reposts[i].innerText)
+    };
+
+    var repostTitles = _.uniq(repostTitles);
+    console.log("Now Playing: " + $("a.playbackTitle__link").text());
+
+    setInterval(function() {
+        if(repostsRemoved == true) {
+          var nowPlaying = $("a.playbackTitle__link").text();
+          for( i = 0; i < repostTitles.length; i++ ) {
+            if(repostTitles[i].indexOf(nowPlaying) > -1) {
+            $(".skipControl__next").trigger("click");
+        }
+      }
+    }
+  }, 800)
+
+  };
+
+
+}
